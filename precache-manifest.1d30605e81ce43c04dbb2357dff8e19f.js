7a0d5461d270535601afbe7718ebf6eea19b9111
Shashwat Awasthi <shashawasthi89@gmail.com>
2019-12-26 14:33:05 +0530

    Updates

---
+++ precache-manifest.1d30605e81ce43c04dbb2357dff8e19f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29d18c5cdbeea2a715d20911d398c75",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "2f21a0035accdcca0a59",
+    "url": "/Todo-List/static/css/main.da17b8d8.chunk.css"
+  },
+  {
+    "revision": "9ecab6bbd2d7d61c2aee",
+    "url": "/Todo-List/static/js/2.97c4eda2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-List/static/js/2.97c4eda2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f21a0035accdcca0a59",
+    "url": "/Todo-List/static/js/main.5aac5edc.chunk.js"
+  },
+  {
+    "revision": "b1dc9f2b2507c645da35",
+    "url": "/Todo-List/static/js/runtime-main.b7b63af8.js"
+  }
+]);
